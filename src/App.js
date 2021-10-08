e948fea887d76f07aa407b849249369ed74ba029
Yoann <maulineyoann@gmail.com>
2021-10-08 12:11:28 +0200

    changement de la barre de scroll

--- src/App.js
+++ src/App.js
@@ -6,15 +6,16 @@ import Products from './components/Products';
 import { productData, productDataTwo } from './components/Products/data';
 import Feature from './components/Feature';
 import Footer from './components/Footer';
+import './App.css';
 
 function App() {
   return (
     <Router>
       <GlobalStyle />
       <Hero />
-      <Products heading='Choisissez la votre' data={productData} />
+      <Products heading="Choisissez la votre" data={productData} />
       <Feature />
-      <Products heading='Nous vous recommandons' data={productDataTwo} />
+      <Products heading="Nous vous recommandons" data={productDataTwo} />
       <Footer />
     </Router>
   );
