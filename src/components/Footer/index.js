8a73d46fb96dc3785f7469d9419a06a92eedb476
Yoann <maulineyoann@gmail.com>
2021-10-08 10:09:14 +0200

    modification

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ import {
   FaInstagram,
   FaYoutube,
   FaTwitter,
-  FaLinkedin
+  FaLinkedin,
 } from 'react-icons/fa';
 import {
   FooterContainer,
@@ -13,7 +13,7 @@ import {
   SocialMediaWrap,
   SocialLogo,
   SocialIcons,
-  SocialIconLink
+  SocialIconLink,
 } from './FooterElements';
 
 const Footer = () => {
@@ -22,25 +22,29 @@ const Footer = () => {
       <FooterWrap>
         <SocialMedia>
           <SocialMediaWrap>
-            <SocialLogo to='/'>Pizza</SocialLogo>
+            <SocialLogo to="/">Pizza</SocialLogo>
             <SocialIcons>
-              <SocialIconLink href='/' target='_blank' aria-label='Facebook'>
+              <SocialIconLink href="/" aria-label="Facebook">
                 <FaFacebook />
               </SocialIconLink>
-              <SocialIconLink href='/' target='_blank' aria-label='Instagram'>
+              <SocialIconLink href="/" aria-label="Instagram">
                 <FaInstagram />
               </SocialIconLink>
-              <SocialIconLink href='/' target='_blank' aria-label='Youtube'>
+              <SocialIconLink href="/" aria-label="Youtube">
                 <FaYoutube />
               </SocialIconLink>
               <SocialIconLink
-                target='_blank'
-                aria-label='Twitter'
-                rel='noopener noreferrer'
+                target="_blank"
+                aria-label="Twitter"
+                rel="noopener noreferrer"
               >
                 <FaTwitter />
               </SocialIconLink>
-              <SocialIconLink href='//www.linkedin.com/in/yoann-mauline/' target='_blank' aria-label='Linkedin'>
+              <SocialIconLink
+                href="//www.linkedin.com/in/yoann-mauline/"
+                target="_blank"
+                aria-label="Linkedin"
+              >
                 <FaLinkedin />
               </SocialIconLink>
             </SocialIcons>
